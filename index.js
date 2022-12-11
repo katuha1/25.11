a21b69e0050e53c9797ec45860dc070a7e6d31ec
katuha1 <103944846+katuha1@users.noreply.github.com>
2022-12-12 00:06:14 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -20,74 +20,62 @@ app.set('view engine', 'pug');
 app.use(express.static("views"));
 // ---
 
-const path_json = path.join(__dirname, "/data.json")
-const work_status = {1: "wait", 2: "active", 3: "complet"}
+const JsonFile = path.join(__dirname, "/data.json")
+const work_status = {1: "wait", 2: "work", 3: "complet"}
 
 function parameters() {
 	return {
-		Task: jsonfile.readFileSync(path_json)
+		listTask: jsonfile.readFileSync(JsonFile)
 	}
 }
 
-function generate_task() {
-	let key = 1;
-	let unpos = [];
-	for (let j=0;j<parameters().Task.length;j++) {
-		unpos.push(parameters().Task[j].task_id);
-	}
-	
-	while(true) {
-		if(unpos.find(i => (i === key))) key++;
-		else return key;
-	}
-}
+app.post('/dashboard/addtask', urlencodedParser, (request, response) => {
+  if (!request.body) return response.sendStatus(400)
+  const data_task = {
+	task_id: parameters().listTask.length+1,
+	head: request.body.head,
+    description: request.body.desc,
+    date: request.body.date,
+    author: request.body.author,
+    statusTask: "wait"
+  };
+  
+  jsonfile.readFile(JsonFile, (error, object) => {
+	  if (error) throw error
+	  object.push(data_task);
+	  jsonfile.writeFile(JsonFile, object, { spaces: 2 }, (error) => {
+		  if (error) throw error;
+	  });
+  });
+  response.redirect(303, '/dashboard')
+});
 
-app.post('/dashboard/addtask', function(request, response) {
-	let data = request.body;
-	let new_data_task = {
-		"task_id": generate_task(),
-		"task_name": data.head,
-		"task_description": data.desc,
-		"task_date": data.date,
-		"task_author": data.author,
-		"task_status": "wait"
-	};
+app.post('/dashboard/deletetask/:id', (request, response) => {
+  jsonfile.readFile(JsonFile, (error, object) => {
+    if (error) throw error
+    for(let i = 0; i < object.length; i++) {
+      if (object[i].task_id == request.params.id) {
+        object.splice(i, 1)
+      }
+    }
 	
-	jsonfile.readFile(path_json, (error, object) => {
-		if (error) throw error
-		object.push(new_data_task);
-		jsonfile.writeFile(path_json, object, { spaces: 2 }, (error) => {
-			if (error) throw error;
-		});
-	});
-	response.redirect(303, '/dashboard');
-})
-
-app.post('/dashboard/removetask/', function (request, response) {
-	jsonfile.readFile(path_json, (error, object) => {
-		if (error) throw error
-		for(let i = 0; i < object.length; i++) {
-			if (object[i].task_status == "complet") {
-				object.splice(i, 1)
-				i--;
-			}
-		}
-    jsonfile.writeFile(path_json, object, { spaces: 2 }, (error) => {
+    jsonfile.writeFile(JsonFile, object, { spaces: 2 }, (error) => {
       if (error) throw error;
     });
   });
   response.redirect(303, '/dashboard');
 });
 
-app.post('/api/editstatus', function (request, response) {
-	jsonfile.readFile(path_json, (error, object) => {
+
+app.post('/api/add', function (request, response) {
+	jsonfile.readFile(JsonFile, (error, object) => {
 		if (error) throw error
 		for(let i = 0; i < object.length; i++) {
 			if (object[i].task_id == request.body.block_id) {
-				object[i]["task_status"] = work_status[request.body.field];
+				object[i]["statusTask"] = work_status[request.body.field];
 			}
 		}
-		jsonfile.writeFile(path_json, object, { spaces: 2 }, (error) => {
+		jsonfile.writeFile(JsonFile, object, { spaces: 2 }, (error) => {
 			if (error) throw error;
 		});
 	});
